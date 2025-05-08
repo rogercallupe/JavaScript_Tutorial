10f34302679254c198fc36ec08da03675bdb8aba
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-08 14:16:25 -0500

    JavaScript_Tutorial-chapter_2.6-array

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,3 +1,14 @@
 let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-console.log(ninjas[1]);
+// ninjas[1] = 'ken';
+// console.log(ninjas[1]);
+
+// let  ages = [20, 25, 30, 35];
+// console.log(ages[2]);
+// let random = ['shaun', 'crystal', 30, 20];
+// console.log(random);
+
+// console.log(ninjas.length);
+
+// arrays methods
+
