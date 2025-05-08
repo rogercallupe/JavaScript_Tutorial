69190b280d77be28af72cc9def204e879064b26d
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-08 14:02:23 -0500

    JavaScript_Tutorial-chapter_2.5-htm-template

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -4,10 +4,20 @@ const author = 'Mario';
 const likes = 30;
 
 // concatenation way
-let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-console.log(result);
+// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
+// console.log(result);
 
 // template string way
 
+let result = `The blog called ${title} by ${author} has ${likes} likes`;
+console.log(result);
 
 // create html template
+let html = `
+  <h2>${title}</h2>
+  <p>By ${author}</p>
+  <span>This blog has ${likes} likes</span>
+`;
+
+console.log(html);
+
