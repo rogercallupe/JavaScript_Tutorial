07bcff5f8c41633eb05cfd8818dadc596699fae8
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-08 11:09:40 -0500

    JavaScript_Tutorial-chapter_2

---
+++ chapter_2/sandbox.js
@@ -0,0 +1,14 @@
+let age = 25;
+let year = 2023;
+
+// log thing to console
+console.log(age, year);
+
+age = 30; 
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+// var score = 75;
+// console.log(score);
