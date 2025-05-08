71a31da190998bc998206e6f93d6f57bd115dc24
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-08 15:45:11 -0500

    JavaScript_Tutorial-chapter_2.8-Booleans-Operators

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,7 +1,33 @@
 // booleans & comparisons
-console.log(true, false, "true", "false") // true and false are booleans, "true" and "false" are strings
+// console.log(true, false, "true", "false") // true and false are booleans, "true" and "false" are strings
 
 // methods can return booleans
+let email = 'luigi@thenetninja.co.uk';
+let names = ['mario', 'luigi', 'toad'];
 
+
+// let result = email.includes('@');
+// let result = names.includes('mario');
+
+// console.log(result);
 
 // comparison operators
+
+let age = 25;
+
+// console.log(age == 25); // true
+// console.log(age == 30); // false
+// console.log(age != 30); // true
+// console.log(age > 20); // true
+// console.log(age < 20); // false
+// console.log(age <= 25); // true
+// console.log(age >= 25); // true
+
+let name = 'shaun';
+
+console.log(name == 'shaun'); // true
+console.log(name == 'Shaun'); // false
+console.log(name > 'crystal'); // true, because 's' comes after 'c' in the alphabet
+console.log(name > 'Shaun'); // true,
+console.log(name > 'Crystal');// true
+console.log(name > 'Zion');//true, because 's' lowercase comes after 'Z' uppercase in the ASCII table
