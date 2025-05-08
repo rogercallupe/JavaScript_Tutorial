1ad532749877d2a28f00bddf24f0f4246e46a8bf
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-08 14:06:20 -0500

    JavaScript_Tutorial-chapter_2.6

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,23 +1,3 @@
-// template string
-const title = 'Best reads of 2019';
-const author = 'Mario';
-const likes = 30;
+let ninjas = ['shaun', 'ryu', 'chun-li'];
 
-// concatenation way
-// let result = 'The blog called ' + title + ' by ' + author + ' has ' + likes + ' likes';
-// console.log(result);
-
-// template string way
-
-let result = `The blog called ${title} by ${author} has ${likes} likes`;
-console.log(result);
-
-// create html template
-let html = `
-  <h2>${title}</h2>
-  <p>By ${author}</p>
-  <span>This blog has ${likes} likes</span>
-`;
-
-console.log(html);
-
+console.log(ninjas[1]);
