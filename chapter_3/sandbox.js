c3a6ee9fd2d9c5edb9543f1ed95aa98f4a0da36b
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-09 09:38:18 -0500

    JavaScript_Tutorial-chapter_3-loops-while2

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,11 +1,17 @@
 // while loops
-
+const names = ['shaun', 'mario', 'luigi'];
 // for (let i = 0; i < 5; i++) {
 //     console.log('in loop: ', i);
 // }
 
+// let i = 0;
+// while (i < 5) {
+//     console.log('in loop: ', i);
+//     i++;
+// }
+
 let i = 0;
-while (i < 5) {
-    console.log('in loop: ', i);
+while (i < names.length){
+    console.log(names[i]);
     i++;
 }
