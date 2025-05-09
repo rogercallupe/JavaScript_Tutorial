9ba1782ffd0a010e6015472f3edb4d89b3a74a9e
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-09 10:49:14 -0500

    JavaScript_Tutorial-chapter_3-else if

--- chapter_3/sandbox.js
+++ chapter_3/sandbox.js
@@ -1,17 +1,17 @@
-// if statements
-// const age = 20;
+// else if statements
 
-// if(age > 20) {
-//     console.log('You are over 20 years old');
+const password = 'p@ssword1';
+
+// if(password.length >= 8) {
+//     console.log('That password is long enough!');
+// } else {
+//     console.log('Password is not long enough');
 // }
 
-// const ninjas = ['shaun', 'ryu', 'chun-li', 'yoshi'];
-// if(ninjas.length > 3) {
-//     console.log("That's a lot of ninjas");
-// }
-
-const password = 'p@ss';
-
-if(password.length >= 8) {
+if(password.length >= 12) {
+    console.log('That password is mighty strong!');
+} else if(password.length >= 8) {
     console.log('That password is long enough!');
+} else {
+    console.log('Password is not long enough');
 }
