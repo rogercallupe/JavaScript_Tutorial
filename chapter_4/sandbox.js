78f81a18fd039525d30a14f282c31a80a388feb6
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-12 08:39:22 -0500

    JavaScript_Tutorial-chapter_4-fuction-declaration

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,11 +1,24 @@
+
+
+// function expression
+
+const speak = function(){
+    console.log('good day!');
+};
+        
+        
+ greet(); // but  works with hoisting in declaration
+ greet(); // but works with hoisting in declaration
+ greet(); // but works with hoisting in declaration 
+
+// speak();
+// speak();
+// speak();
+
+
+
 // function declaration
 
 function  greet(){
     console.log('hello there');
 }
-
-// function expression
-
-greet();
-greet();
-greet();
