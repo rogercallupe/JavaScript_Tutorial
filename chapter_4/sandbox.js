ffc4ea5d5ade41510fb4ef43de826599eb4fc8fd
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-12 09:15:58 -0500

    JavaScript_Tutorial-chapter_4-fuction-return values

--- chapter_4/sandbox.js
+++ chapter_4/sandbox.js
@@ -1,7 +1,19 @@
-// arguments & parameters
+// returning values
 
-const speak = function(name, time) {
-  console.log(`good ${time} ${name}`);
+// const speak = function(name = 'luigi', time = 'night') {
+//   console.log(`good ${time} ${name}`);
+// };
+
+const calcArea = function(radius) {
+  let area =  3.14 * radius**2;
+  return area;
+  //console.log(area);
+}
+
+const area = calcArea(5);
+console.log(area);
+
+const calcVol = function(area) {
 };
 
-speak('mario', 'morning');
+calcVol(area)
