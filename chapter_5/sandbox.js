a68ae256d68c499f6062b59264b8aa0d9b24094e
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-13 09:17:38 -0500

    JavaScript_Tutorial-chapter_5-object-functions-methods2

--- chapter_5/sandbox.js
+++ chapter_5/sandbox.js
@@ -6,24 +6,33 @@ let user = {
     email: 'crystal@thenetninja.co.uk',
     location: 'berlin',
     blogs: ['why mac & cheese rules', '10 things to make with marmite'],
-    login: function() {
+    login() {
+    //login: function() { // this is the same as the above regular function
         console.log('the user logged in');
     },
-    logout: function() {
+    logout() {
+    // logout: function() { // this is the same as the above regular function
         console.log('the user logged out');
     },
-    logBlogs: function(){
+    // logBlogs: () => { //    this is an arrow function not working in because use 'this'
+    logBlogs() { // this is a regular function
+    //logBlogs: function(){
 
     // console.log(blogs); // this will not work because blogs is not defined in this scope
     //    this.blogs  // this will work because this refers to the user object windows
     // console.log(this); // this will work because this refers to complete the user object 
     // console.log(this.blogs); // this will work because this refers to the blogs property of the user object
+    
+    
     console.log('this user has written the following blogs:');
     this.blogs.forEach(blog => {
     console.log(blog);
-    })
+     })
    
-}
+
+    console.log(this);
+
+    }    
 };
 
 user.logBlogs(); // calling the method
