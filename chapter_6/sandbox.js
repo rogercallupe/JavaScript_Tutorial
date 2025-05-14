8135fbbcd626db1c5bfe3600a07fdbbc298bccb4
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-14 10:24:40 -0500

    JavaScript_Tutorial-chapter_6-Selector2

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -4,5 +4,11 @@
 //console.log(para);
 
 const paras = document.querySelectorAll('p');
+const errors = document.querySelectorAll('.error');
 
-console.log(paras[2]);
+// paras.forEach(para => {
+//     console.log(para);
+// });
+//console.log(paras[2]);
+
+console.log(errors);
