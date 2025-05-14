907922bfbe98a3c9c7c3f4da4e57db84d59da4c9
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-14 14:59:56 -0500

    JavaScript_Tutorial-chapter_6-innerText-textContent-add

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -7,10 +7,19 @@
 
 const paras = document.querySelectorAll('p');
 
-paras.forEach(p => {
-    console.log(p.innerText); // just show all txt visible
-});
+// paras.forEach(p => {
+//     console.log(p.innerText); // just show all txt visible
+// });
+
+// paras.forEach(p => {
+//     console.log(p.textContent); // show all text content
+// });
 
 paras.forEach(p => {
-    console.log(p.textContent);
+    if(p.textContent.includes('error')){
+        p.classList.add('error');
+    }
+    if(p.textContent.includes('success')){
+        p.classList.add('success');
+    }
 });
