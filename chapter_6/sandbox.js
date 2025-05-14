e1ba8ceed52d03d67f6b6e1b02115de986782fbf
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-14 11:37:22 -0500

    JavaScript_Tutorial-chapter_6-innerText-innerHTML

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -5,8 +5,18 @@ const para = document.querySelector('p');
 
 const paras = document.querySelectorAll('p');
 
-paras.forEach(para =>{
-    console.log(para.innerText);
-    para.innerText += ' new txt';
-});
+// paras.forEach(para =>{
+//     console.log(para.innerText);
+//     para.innerText += ' new txt';
+// });
 
+const content = document.querySelector('.content');
+
+// console.log(content.innerHTML);
+// content.innerHTML += '<h2> THIS IS A NEW H2</h2>';
+
+const people = ['mario', 'luigi', 'yoshi'];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+});
