a183027b35eca18788f1a2302a9fec9cb47cb9a3
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-14 13:24:02 -0500

    JavaScript_Tutorial-chapter_6-setAttribute

--- chapter_6/sandbox.js
+++ chapter_6/sandbox.js
@@ -1,22 +1,11 @@
-const para = document.querySelector('p');
+const link = document.querySelector('a');
 
-//console.log(para.innerText);
-//para.innerText += ' ninjas are awesome!';
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerText = 'The Net Ninja Website';
 
-const paras = document.querySelectorAll('p');
+const mssg = document.querySelector('p');
 
-// paras.forEach(para =>{
-//     console.log(para.innerText);
-//     para.innerText += ' new txt';
-// });
-
-const content = document.querySelector('.content');
-
-// console.log(content.innerHTML);
-// content.innerHTML += '<h2> THIS IS A NEW H2</h2>';
-
-const people = ['mario', 'luigi', 'yoshi'];
-
-people.forEach(person => {
-    content.innerHTML += `<p>${person}</p>`;
-});
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color:green');
