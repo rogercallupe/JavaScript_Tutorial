25a470362b221c9bed36959c8b04df1e69a0ffb4
rcallupecoronado <162754651+rcallupecoronado@users.noreply.github.com>
2025-05-14 09:58:48 -0500

    JavaScript_Tutorial-chapter_6-Selector

---
+++ chapter_6/sandbox.js
@@ -0,0 +1,8 @@
+//const para = document.querySelector('p');
+//const para = document.querySelector('body > h1:nth-child(1)');
+
+//console.log(para);
+
+const paras = document.querySelectorAll('p');
+
+console.log(paras[2]);
